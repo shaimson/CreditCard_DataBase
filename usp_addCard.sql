f9a7cbd20351a48d87e3f5fa1e9f356888ce8f4c
develper613 <87280497+develper613@users.noreply.github.com>
2022-01-03 01:00:24 -0500

    Resolving git errors still

--- usp_addCard.sql
+++ usp_addCard.sql
@@ -6,9 +6,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
-CREATE PROCEDURE usp_addCard
+ALTER PROCEDURE usp_addCard
 	@cardNum varchar(16), 
 	@cardType int,
 	@company varchar(45),
@@ -19,10 +17,8 @@ CREATE PROCEDURE usp_addCard
 	@creditLimit decimal(10,2)
 AS
 BEGIN
-
 	begin try
 	SET NOCOUNT ON;
-
 	declare @ccType int
 	select @ccType = TypeID
 	from CreditCardType
@@ -32,7 +28,6 @@ BEGIN
 	select @typeName = [Type] 
 	from CreditCardType
 	where TypeID = @ccType
-
 	if (len(@cardNum) != 16 and @typeName in ('VISA' , 'MASTERCARD', 'DISCOVER')) or (len(@cardNum) != 15 and @typeName like 'AMEX')
 	begin; 
 	throw 80000, 'Invalid Card Number', 1
@@ -41,14 +36,10 @@ BEGIN
 
 	insert into CreditCard(CreditCardNum, [Type], Company, IssueDate, ExpirationDate, CurrentBalance,[Status], CreditLimit)
 	values(@cardNum, @cardType, @company, @issueDate, @expirationDate, @currentBalance, @cardStatus, @creditLimit)
-
 	end try
-
-	begin catch
-
+	begin catch;
 	throw 80001, 'Card could not be processed', 1;
-
 	end catch
-    
+
 END
 GO
