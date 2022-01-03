d1a0183c6739fa6019c8008d174355615c1289e6
develper613 <87280497+develper613@users.noreply.github.com>
2022-01-03 18:52:25 -0500

    Added more insertions

---
+++ PurchaseVendors Insertion.sql
@@ -0,0 +1,45 @@
+insert into Vendors(VendorID, VendorName, VendorStreet, VendorCity, VendorState, VendorZip)
+values(1324, 'Rosie''s Boutique', '17 Maple Lane', 'Atlanta', 'GA', 30312)
+
+insert into Vendors(VendorID, VendorName, VendorStreet, VendorCity, VendorState, VendorZip)
+values(5768, 'Tony Tacos', '917 West 81st Street', 'New York', 'NY', 10001)
+
+insert into Vendors(VendorID, VendorName, VendorStreet, VendorCity, VendorState, VendorZip)
+values(9023, 'Wholesale Market', '803 Jersey Grove', 'Edison', 'NJ', 08818)
+
+insert into Vendors(VendorID, VendorName, VendorStreet, VendorCity, VendorState, VendorZip)
+values(2641, 'Con-Ed', '1423 Shore Drive', 'Greensboro', 'NC', 27401)
+
+insert into Vendors(VendorID, VendorName, VendorStreet, VendorCity, VendorState, VendorZip)
+values(6721, 'Air BnB', '888 Brannan Street', 'San Frincisco', 'CA', 94103)
+
+insert into Vendors(VendorID, VendorName, VendorStreet, VendorCity, VendorState, VendorZip)
+values(9223, 'Plaza Auto Mall', '1786 Coney Island Avenue', 'Brooklyn', 'NY', 11230)
+
+
+
+insert into PurchaseTypes(ValidType)
+values('FOOD')
+insert into PurchaseTypes(ValidType)
+values('LODGING')
+insert into PurchaseTypes(ValidType)
+values('UTILITY')
+insert into PurchaseTypes(ValidType)
+values('TRAVEL')
+insert into PurchaseTypes(ValidType)
+values('CLOTHING')
+insert into PurchaseTypes(ValidType)
+values('RESTAURANT')
+insert into PurchaseTypes(ValidType)
+values('CAR')
+insert into PurchaseTypes(ValidType)
+values('GROCERIES')
+
+select *
+from PurchaseTypes
+
+exec usp_Purchase 1500.34, '2020-05-22', 1324, 5, 5424678911225577   
+exec usp_Purchase 350.00, '2019-12-05', 2641,3,342467891122558
+exec usp_Purchase 259.00, '2020-01-14', 9023, 8, 6424678911225512
+exec usp_Purchase 12000.99, '2021-04-15', 9223, 7, 6424678911225512
+exec usp_Purchase 67.93, '2019-12-30', 5768, 6, 5424678911225577
